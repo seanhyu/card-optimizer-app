a0bce1d8c512314e2000e99a935309caf66249b6
Sean <seanhyu@gmail.com>
2024-05-22 21:11:10 -0400

    Initial Commit

---
+++ Card Optimizer App/Card_Optimizer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Card_Optimizer_AppApp.swift
+//  Card Optimizer App
+//
+//  Created by Sean Yu on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Card_Optimizer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
